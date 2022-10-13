f2206c445446b3e26b8a1b8a3f36f27d53a3a88f
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-13 03:33:32 -0700

    Task 4 solvedgit add .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * @void: Function argument
+ * Return: 0 if the code runs successfuly. 1 otherwise
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
