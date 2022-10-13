cde6dc8985dbc261fd8f898deaa5174ad488bcb8
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-13 03:39:13 -0700

    Task 5 solvedgit add .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- use printf to output a string
+ * @void: function parameter
+ * Return: 0 if successful. 1 otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
