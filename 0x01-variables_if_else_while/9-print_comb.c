f92d3431f921b868ebe4083829171a13a31c50dd
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-14 05:17:35 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: parameter
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
