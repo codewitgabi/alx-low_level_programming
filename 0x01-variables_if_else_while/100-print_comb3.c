81cbbe3669c289b258f3e5cc7bcd01ae7c81649d
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-14 13:55:47 -0700

    Task 10 solved

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,4 +11,20 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0 
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 && j != 58)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
