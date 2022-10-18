43079181ffe5f11ca0f75a98be6ec5977e663509
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-18 10:03:24 -0700

    Task 7 solved

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,6 @@
 
 int print_last_digit(int n)
 {
-	_putchar(n > 1 ? n % 10 + 48 : (n * -1) % 10 + 48);
-	return (n > 1 ? n % 10 : (n * -1) % 10);
+	_putchar(n > 0 ? n % 10 + 48 : (n % 10) * -1 + 48);
+	return (n > 0 ? n % 10 : (n * -1) % 10);
 }
