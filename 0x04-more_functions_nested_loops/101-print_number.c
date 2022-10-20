a20a9e50c51f42b5d8309c41e6f9b8cfcb5fffa2
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-20 14:10:21 -0700

    Solved all tasks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,17 @@
 
 void print_number(int n)
 {
+	unsigned int a;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		a = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	else
+		a = n;
+
+	if (a / 10)
+		print_number(a / 10);
+	_putchar(a % 10 + '0');
 }
