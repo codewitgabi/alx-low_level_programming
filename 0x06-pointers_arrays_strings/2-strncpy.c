29a5e994bbb180a76b2a56400fbec14261becf09
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-26 03:21:37 -0700

    Solved all tasks

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * _strncpy - copies src into dest and characters must
+ * be equal to n.
+ * @dest: string to be copied into
+ * @src: string to be copied from
+ * @n: number of characters to be copied
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
