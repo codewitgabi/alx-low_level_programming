228ba7edeb5fa946e9bf36632341b2219040252a
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-26 10:25:29 -0700

    Solved all tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,13 +7,13 @@
 
 void print_number(int n)
 {
-	int count, inc, n2;
+	int count, inc;
 
 	count = 1;
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n *= -1;
 	}
 	for (inc = 10; n % inc < n; inc *= 10)
@@ -22,7 +22,7 @@ void print_number(int n)
 
 	while (count > 0)
 	{
-		putchar((n / inc) % 10 + '0');
+		_putchar((n / inc) % 10 + '0');
 		count--;
 		inc /= 10;
 	}
