9795127691c739bd5611302aebb7a8a75c06dfc6
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-26 17:05:39 -0700

    Solved all tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array in place
+ * @a: array to reverse
+ * @n: length of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp, i, mid;
+
+	i = n;
+	mid = (n - 1) / 2;
+
+	for (i = mid; i >= 0; i--)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
