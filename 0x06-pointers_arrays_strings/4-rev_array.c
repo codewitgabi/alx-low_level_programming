52fbf4fac69db86cae441176ea337bdc654ab99a
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-10-27 09:10:45 -0700

    Solved all tasks

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,29 @@
 
 void reverse_array(int *a, int n)
 {
-	int temp, i, mid;
+	int x, y, z;
 
-	i = n;
-	mid = (n - 1) / 2;
-
-	for (i = mid; i >= 0; i--)
+	for (x = 0; x < n - 1; x++)
 	{
-		temp = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = temp;
+		for (y = x + 1; y > 0; y--)
+		{
+			z = a[y];
+			a[y] = a[y - 1];
+			a[y - 1] = z;
+		}
 	}
+	/**
+	* The below block of code works but betty refused
+	* to check it. Not really sure why though but
+	* for other cases, this should also be ok for you.
+	* i = n;
+	* mid = (n - 1) / 2;
+
+	* for (i = mid; i >= 0; i--)
+	* {
+	* temp = a[i];
+	* a[i] = a[n - 1 - i];
+	* a[n - 1 - i] = temp;
+	* }
+	*/
 }
