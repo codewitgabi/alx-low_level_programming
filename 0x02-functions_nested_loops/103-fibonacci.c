af74a5695d9562319f3713532cc745297e81be86
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-01 15:08:29 -0700

    Updated 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,28 +8,17 @@
 
 int main(void)
 {
-	ong int a;
-	long int b;
-	long int c;
-	long int temp;
-	long int n;
+	unsigned int a = 1, b = 1, z, count = 0;
 
-	n = 0;
-	a = 0;
-	b = 1;
-	c = 0;
-
-	while (c < 50 && b < 3000000)
+	while (b < 4000000)
 	{
-		temp = a;
-		a = b;
-		b = a + temp;
-
 		if (b % 2 == 0)
-			n += b;
-		c++;
+			count += b;
+		z = a;
+		a = b;
+		b = a + z;
 	}
-	printf("%ld\n", n);
+	printf("%u\n", b);
 
 	return (0);
 }
