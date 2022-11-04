da2c5b40e75cbca032fa421d4593dba67167454e
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-04 12:20:49 -0700

    Solved all tasks

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,7 +8,7 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, firstNum;
+	int sign = 1, resp = 0, firstNum, i;
 
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
@@ -18,7 +18,7 @@ int _atoi(char *s)
 		}
 	}
 
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
 		resp *= 10;
 		resp += (s[i] - 48);
