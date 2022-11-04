a45d45d90726c1da485bb0def176065249247e37
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-04 11:05:05 -0700

    Solved all tasks

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _islower - checks if a number is in lowercase
+ * @c: character
+ * Return: 0
+ */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ * _isalpha - checks if a character is an alphabet
+ * @c: param
+ * Return: 0
+ */
+
 int _isalpha(int c)
 {
-	(void)c;
-
+	if (c >= '0' && c<= '9')
+		return (1);
 	return (0);
 }
