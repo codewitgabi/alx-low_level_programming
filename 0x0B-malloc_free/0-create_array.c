7e592adbc13aaebb416122574a5d1463b3b565ae
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-07 10:44:06 -0800

    Solved all tasks

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(array) * size);
+
+	array = malloc(sizeof(char) * size);
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
 
