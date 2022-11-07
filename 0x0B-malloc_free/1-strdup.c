88ba093d1beae8d9d12be20bd197a6c202d3a02e
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-07 11:36:34 -0800

    Solved all tasks

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: param
+ * Return pointer to str
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int c = 0, j;
+
+	if (str == NULL)
+		retrn (NULL);
+
+	while (*(str + c))
+		c++;
+
+	s = malloc(c);
+
+	for (j = 0; j < c; j++)
+		s[j] = str[j];
+
+	s[j] = '\0';
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat -  concatenates two strings.
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
+ * Return: pointer to concatenated string
+ */
+
+char *_strdup(char *str)
+{
+	char *s3;
+	int c = 0, i = 0, j;
+
+	while (*(s1 + c))
+		c++;
+	while (*(s2 + i))
+		i++;
+	s3 = malloc(c + i);
+
+	for (j = 0; j < c; j++)
+		s3[j] = s1[j];
+
+	s3[j] = '\0';
+
+	return (s3);
+}
