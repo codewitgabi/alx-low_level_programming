16507064c8794e219ce47354ad7aed9314979fb5
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-08 14:05:50 -0800

    Solved all tasks

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -63,6 +63,10 @@ char **strtow(char *str)
 	if (words == 0)
 		return (NULL);
 
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
 	for (w = 0; w < words; w++)
 	{
 		while (str[index] == ' ')
