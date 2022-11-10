8906bc3b860480953f4e5c82786dfe60030c5ad7
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-10 13:11:02 -0800

    Solved all tasks

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,47 +1,41 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @old_size: param
- * @new_size: param
- * Return: void pointer
+ * _realloc -  reallocates a memory block using malloc and free
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ptr_copy, *filler;
-	unsigned int index;
+	char *clone, *relloc;
+	unsigned int i;
+
+	if (ptr != NULL)
+		clone = ptr;
+	else
+		return (malloc(new_size));
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr == NULL)
-	{
-		mem = malloc(new_size);
-		if (mem == NULL)
-			return (NULL);
-		return (mem);
-	}
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (NULL);
+		return (0);
 	}
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
 
-	if (mem == NULL)
+	relloc = malloc(new_size);
+
+	if (relloc == NULL)
+		return (0);
+
+	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		free(ptr);
-		return (NULL);
+		*(relloc + i) = clone[i];
 	}
-
-	filler = mem;
-
-	for (index = 0; index < old_size && index < new_size; index++)
-		filler[index] = *ptr_copy++;
 
 	free(ptr);
-	return (mem);
+	return (relloc);
 }
