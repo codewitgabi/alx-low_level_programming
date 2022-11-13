dfb4ff4685c153d204546578bc19f7119353c981
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-12 16:24:23 -0800

    Updated code snippet

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (*(src + i) && i < n)
-	{
+	for (i = 0; *(src + i) && i < n; i++)
 		dest[i] = src[i];
-		i++;
-	}
 
-	dest[i] = '\0';
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
