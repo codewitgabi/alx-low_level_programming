50e7645b43f140b40c22bcafeea5fe039e75d6b6
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-14 08:49:50 -0800

    Solved all tasks

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog- creates a dog instance
+ * @d: pointer to struct onject
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
