0348ae6fe847305c1f1affa077f39e05223ba7f9
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2022-11-17 00:18:30 -0800

    Solved all tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints all nunbers passed as optional arg.
+ * @separator: character to separate nunbers to be printed
+ * @n: number of args
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	if (separator != NULL)
+	{
+		va_start(arg, n);
+
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(arg, int));
+			if (i != n - 1)
+				printf("%s", separator);
+		}
+		va_end(arg);
+		printf("\n");
+	}
+}
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef HEADER
 #define HEADER
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
